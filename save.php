9afd8e7b538ce017d291d08aef0db0a582e0b6fd
naim187naim <naimalime.pro@gmail.com>
2026-02-20 17:32:56 +0100

    Dernière mise à jour save.php pour les mails

--- save.php
+++ save.php
@@ -1,18 +1,41 @@
 <?php
+// 1. Connexion à la base de données
 $conn = new mysqli("localhost", "root", "", "demande_noelie");
+
+// Ton adresse mail de réception
 $mon_mail = "naimalime.pro@gmail.com";
 
-if (isset($_POST['choix'])) {
-    $choix = $conn->real_escape_string($_POST['choix']);
-    $conn->query("INSERT INTO reponses (choix) VALUES ('$choix')");
+// 2. Récupération des données (Choix ou Message)
+$choix = $_POST['choix'] ?? null;
+$message_texte = $_POST['message_texte'] ?? null;
+
+// --- CAS A : Enregistrement d'un choix (Oui, Non, Ami, etc.) ---
+if ($choix) {
+    $choix_clean = $conn->real_escape_string($choix);
     
-    // Cette ligne envoie le mail du choix
-    shell_exec("echo 'Subject: Reponse de Noelie\n\nElle a choisi : $choix' | ssmtp $mon_mail");
+    // Sauvegarde en base de données
+    $conn->query("INSERT INTO reponses (choix) VALUES ('$choix_clean')");
+    
+    // Envoi du mail d'alerte
+    $sujet = "Reponse de Noelie";
+    $contenu = "Salut ! Noelie a choisi l'option : " . $choix;
+    
+    // Commande SSMTP
+    shell_exec("echo 'Subject: $sujet\n\n$contenu' | ssmtp $mon_mail");
 }
 
-if (isset($_POST['message_texte'])) {
-    $msg = $_POST['message_texte'];
-    // Cette ligne envoie le message écrit
-    shell_exec("echo 'Subject: Message de Noelie\n\nContenu : $msg' | ssmtp $mon_mail");
+// --- CAS B : Envoi d'un message libre (Page verte) ---
+if ($message_texte) {
+    $msg_clean = $conn->real_escape_string($message_texte);
+    
+    // On prépare le mail pour le message
+    $sujet = "Nouveau message de Noelie";
+    $contenu = "Noelie t'a laisse ce message sur le site :\n\n" . $message_texte;
+    
+    // Commande SSMTP
+    shell_exec("echo 'Subject: $sujet\n\n$contenu' | ssmtp $mon_mail");
 }
+
+// Optionnel : Retourner une réponse au JavaScript
+echo json_encode(["status" => "success"]);
 ?>

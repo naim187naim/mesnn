83d4bd7ce0eafa04ff08c7cf09601e6c12183e6e
naim187naim <naimalime.pro@gmail.com>
2026-02-20 17:25:52 +0100

    Système d'email opérationnel et testé

--- save.php
+++ save.php
@@ -2,22 +2,17 @@
 $conn = new mysqli("localhost", "root", "", "demande_noelie");
 $mon_mail = "naimalime.pro@gmail.com";
 
-// Enregistrement du choix
 if (isset($_POST['choix'])) {
     $choix = $conn->real_escape_string($_POST['choix']);
     $conn->query("INSERT INTO reponses (choix) VALUES ('$choix')");
     
-    // Commande pour envoyer le mail
-    $sujet = "Reponse de Noelie";
-    $contenu = "Elle a repondu : " . $choix;
-    shell_exec("echo 'Subject: $sujet\n\n$contenu' | ssmtp $mon_mail");
+    // Cette ligne envoie le mail du choix
+    shell_exec("echo 'Subject: Reponse de Noelie\n\nElle a choisi : $choix' | ssmtp $mon_mail");
 }
 
-// Enregistrement du message
 if (isset($_POST['message_texte'])) {
     $msg = $_POST['message_texte'];
-    $sujet = "Nouveau message de Noelie";
-    $contenu = "Message : " . $msg;
-    shell_exec("echo 'Subject: $sujet\n\n$contenu' | ssmtp $mon_mail");
+    // Cette ligne envoie le message écrit
+    shell_exec("echo 'Subject: Message de Noelie\n\nContenu : $msg' | ssmtp $mon_mail");
 }
 ?>

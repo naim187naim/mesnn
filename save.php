74ba24e4aa949920b296550900b53558c8e6bc6f
naim187naim <naimalime.pro@gmail.com>
2026-02-20 17:12:54 +0100

    Configuration de l'envoi de mail

--- save.php
+++ save.php
@@ -1,25 +1,33 @@
 <?php
+// Connexion à la base de données
 $conn = new mysqli("localhost", "root", "", "demande_noelie");
+
+// Ton adresse mail où tu veux recevoir les notifications
 $mon_mail = "naimalime.pro@gmail.com";
 
-// 1. Sauvegarde du choix Oui/Non
+// --- CAS 1 : Enregistrement du choix (Oui, Non, Ami, etc.) ---
 if (isset($_POST['choix'])) {
     $choix = $conn->real_escape_string($_POST['choix']);
+    
+    // On insère dans la base de données
     $conn->query("INSERT INTO reponses (choix) VALUES ('$choix')");
     
-    // Notification mail du choix
+    // On prépare l'email
     $sujet = "Reponse de Noelie";
-    $contenu = "Elle a repondu : " . $choix;
-    shell_exec("echo 'Subject: $sujet\n\n$contenu' | ssmtp $mon_mail");
+    $message = "Salut ! Noélie vient de répondre à ton site.\n\nChoix : " . $choix;
+    
+    // Envoi du mail via le serveur (ssmtp doit être configuré)
+    shell_exec("echo 'Subject: $sujet\n\n$message' | ssmtp $mon_mail");
 }
 
-// 2. Envoi du message libre
+// --- CAS 2 : Envoi du message libre ---
 if (isset($_POST['message_texte'])) {
-    $msg = $_POST['message_texte'];
-    $sujet = "Nouveau message de Noelie";
-    $contenu = "Noelie t'a laisse un message :\n\n" . $msg;
+    $msg = $conn->real_escape_string($_POST['message_texte']);
     
-    // Envoi via SSMTP
-    shell_exec("echo 'Subject: $sujet\n\n$contenu' | ssmtp $mon_mail");
+    $sujet = "Nouveau message de Noelie";
+    $message = "Noélie t'a laissé un petit mot :\n\n" . $msg;
+    
+    // Envoi du mail
+    shell_exec("echo 'Subject: $sujet\n\n$message' | ssmtp $mon_mail");
 }
 ?>
